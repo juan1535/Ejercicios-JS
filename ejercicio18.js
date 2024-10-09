2ae8fa176fe3e5e04d0796a16236f2b644a29950
Juan Franco <JuanDFrancoM15@gmail.com>
2024-10-08 21:27:55 -0500

    Resolución de algunos ejercicios JS

--- ejercicio18.js
+++ ejercicio18.js
@@ -2,8 +2,10 @@ let a = parseFloat(prompt("Valor a"));
 let b = parseFloat(prompt("Valor b"));
 let c = parseFloat(prompt("Valor c"));
 
-function calculo(a, b, c) {
+function calculo(a, b, c) {  // una variable en este caso; "resultado" no puede estar definida dentro de una función, en este caso; "calculo" porque no está disponible fuera de ella. 
     let resultado = (a + 7 * c) / (b + 2 - a) + 2 * b;
+    return resultado;
 }
 
-calculo(a, b, c);
+let resultado = calculo(a,b,c);
+alert(`El volumen de la esfera es: ${resultado}`)
--- ejercicio19.js
+++ ejercicio19.js
@@ -2,11 +2,11 @@ let radio = parseFloat(prompt("Ingrese el radio del cilindro"));
 let altura = parseFloat(prompt("Ingrese la altura del radio"));
 
 function calculo(r, a) {
-    // let area = 2 * Math.PI * r * (r + a)
+    let area = 2 * Math.PI * r * (r + a)
     let volumen = Math.PI * r ** 2 * a;
 
     alert(`El area del cilindro es: ${area}
-        El volumen del cilindro es: ${volumen}`)
+        El volumen del cilindro es: ${volumen}`);
 }
 
 calculo(radio, altura)
